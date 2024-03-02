99430289a29f8bfb12fd6918213eb7d3703a8089
Steven Luo <76457271+stevenluo23@users.noreply.github.com>
2024-03-01 18:22:38 -0700

    Create load_from_IMU ability

--- tracker.py
+++ tracker.py
@@ -34,6 +34,33 @@ lat_b = 34.0522
 lon_b = -118.2437
 alt_b = 500
 
-azimuth, elevation = calculate_angles(lat_a, lon_a, alt_a, lat_b, lon_b, alt_b)
+data1 = {
+    "lat": 37.7749,
+    "lon": -122.4194,
+    "alt": 0
+}
+
+data2 = {
+    "lat": 34.0522,
+    "lon": -118.2437,
+    "alt": 500,
+}
+
+def load_from_IMU(data1, data2):
+    # data1 is location of the ground station (possibly constant)
+    # data2 is the location of the IMU onboard satellite
+    # assume data comes in as a dictionary
+    lat_a = data1["lat"]
+    lon_a = data1["lon"]
+    alt_a = data1["alt"]
+    lat_b = data2["lat"]
+    lon_b = data2["lon"]
+    alt_b = data2["alt"]
+    return (lat_a, lon_a, alt_a, lat_b, lon_b, alt_b)
+    
+while (true):
+    lat_a, lon_a, alt_a, lat_b, lon_b, alt_b = load_from_IMU(data1, data2)
+    azimuth, elevation = calculate_angles(lat_a, lon_a, alt_a, lat_b, lon_b, alt_b)
+
 print(f"Azimuth: {azimuth} degrees")
 print(f"Elevation: {elevation} degrees")
